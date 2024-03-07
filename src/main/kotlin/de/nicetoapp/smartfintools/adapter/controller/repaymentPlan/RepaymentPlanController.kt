717c2c0ff78f3344cea01049e0870144c3006648
Mustafa Gercek <mgercek92@googlemail.com>
2024-03-07 10:12:25 +0100

    clean repayment plan controller

--- src/main/kotlin/de/nicetoapp/smartfintools/adapter/controller/repaymentPlan/RepaymentPlanController.kt
+++ src/main/kotlin/de/nicetoapp/smartfintools/adapter/controller/repaymentPlan/RepaymentPlanController.kt
@@ -3,7 +3,10 @@ package de.nicetoapp.smartfintools.adapter.controller.repaymentPlan
 import de.nicetoapp.smartfintools.domain.model.loan.AnnuityLoan
 import de.nicetoapp.smartfintools.domain.model.repaymentPlan.RepaymentPlanRequest
 import de.nicetoapp.smartfintools.domain.model.repaymentPlan.RepaymentPlanResponse
+import de.nicetoapp.smartfintools.domain.model.repaymentPlan.RepaymentPlanStepsResponse
+import de.nicetoapp.smartfintools.domain.usecase.repaymentPlan.GetMonthlyPaymentStepsUseCase
 import de.nicetoapp.smartfintools.domain.usecase.repaymentPlan.GetRepaymentPlanUseCase
+import de.nicetoapp.smartfintools.domain.usecase.repaymentPlan.GetYearlyPaymentStepsUseCase
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController
 
 @Suppress("unused")
 @RestController
-@RequestMapping("/repayment-plans")
+@RequestMapping("/repayment-plan")
 class RepaymentPlanController {
 
     @PostMapping
@@ -24,5 +27,31 @@ class RepaymentPlanController {
             loanRequest.years
         )
         return GetRepaymentPlanUseCase().execute(loan)
+    }
+
+    @PostMapping("/monthly")
+    fun createMonthlyRepaymentPlan(@RequestBody loanRequest: RepaymentPlanRequest): RepaymentPlanStepsResponse {
+        val loan = AnnuityLoan(
+            loanRequest.startDate,
+            loanRequest.amount,
+            loanRequest.interestRate,
+            loanRequest.amortizationRate,
+            loanRequest.years
+        )
+        val steps = GetMonthlyPaymentStepsUseCase().execute(loan)
+        return RepaymentPlanStepsResponse(steps)
+    }
+
+    @PostMapping("/yearly")
+    fun createYearlyRepaymentPlan(@RequestBody loanRequest: RepaymentPlanRequest): RepaymentPlanStepsResponse {
+        val loan = AnnuityLoan(
+            loanRequest.startDate,
+            loanRequest.amount,
+            loanRequest.interestRate,
+            loanRequest.amortizationRate,
+            loanRequest.years
+        )
+        val steps = GetYearlyPaymentStepsUseCase().execute(loan)
+        return RepaymentPlanStepsResponse(steps)
     }
 }
